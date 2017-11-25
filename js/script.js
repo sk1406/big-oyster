284921792948d5e3f09241d84f148e491ae3bb1b
sk1406 <suzannekeller1@gmail.com>
2017-11-25 12:27:14 -0500

    updates

--- js/script.js
+++ js/script.js
@@ -1,8 +1,40 @@
+// countdown clock for 2035
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 1, 2035 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+// Get todays date and time
+var now = new Date().getTime();
+
+// Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+// Time calculations for days, hours, minutes and seconds
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+// Display the result in the element with id="demo"
+document.getElementById("demo").innerHTML = days + "d " + hours + "h "
++ minutes + "m " + seconds + "s ";
+
+// If the count down is finished, write some text 
+if (distance < 0) {
+clearInterval(x);
+document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
 //accordion cards
 
 $('#myCollapsible').collapse({
   toggle: false
-})
+}) 
 
 //google map
 
@@ -19,5 +51,3 @@ var map;
           zoom: 8
         });
       }
-
-//<button onclick="myFunction()">Click me</button>
